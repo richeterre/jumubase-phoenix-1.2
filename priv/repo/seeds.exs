1752d8abca0f4755b3282424d7ca030e3faaee78
Martin Richter <martin.richter@futurice.com>
2017-03-11 15:20:25 +0100

    Add some hosts to seed file

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,13 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Jumubase.Host
+alias Jumubase.Repo
+
+hosts = [
+  %Host{name: "DS Helsinki", city: "Helsinki", country_code: "FI", time_zone: "Europe/Helsinki"},
+  %Host{name: "DS Stockholm", city: "Stockholm", country_code: "SE", time_zone: "Europe/Stockholm"},
+  %Host{name: "DS Dublin", city: "Dublin", country_code: "IE", time_zone: "Europe/Dublin"}
+]
+hosts |> Enum.each(fn host -> Repo.insert(host) end)
