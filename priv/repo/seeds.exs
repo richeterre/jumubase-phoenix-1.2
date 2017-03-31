cadee88c7265a1f34d61d3085f72d71de8f9adcb
Martin Richter <me@martinrichter.net>
2017-03-31 21:44:49 +0200

    Wrap seeding inserts in one big transaction

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -16,45 +16,47 @@ alias Jumubase.User
 alias Jumubase.Host
 alias Jumubase.Contest
 
-# Create demo users
+Repo.transaction fn ->
+  # Create demo users
 
-admin_changeset = User.registration_changeset(%User{}, %{
-  first_name: "Anna",
-  last_name: "Admin",
-  email: "admin@example.org",
-  password: "secret",
-})
-Repo.insert!(admin_changeset)
+  admin_changeset = User.registration_changeset(%User{}, %{
+    first_name: "Anna",
+    last_name: "Admin",
+    email: "admin@example.org",
+    password: "secret",
+  })
+  Repo.insert!(admin_changeset)
 
-# Create demo hosts
+  # Create demo hosts
 
-host1 = Repo.insert!(%Host{name: "DS Helsinki", city: "Helsinki", country_code: "FI", time_zone: "Europe/Helsinki"})
-host2 = Repo.insert!(%Host{name: "DS Stockholm", city: "Stockholm", country_code: "SE", time_zone: "Europe/Stockholm"})
-host3 = Repo.insert!(%Host{name: "DS Dublin", city: "Dublin", country_code: "IE", time_zone: "Europe/Dublin"})
+  host1 = Repo.insert!(%Host{name: "DS Helsinki", city: "Helsinki", country_code: "FI", time_zone: "Europe/Helsinki"})
+  host2 = Repo.insert!(%Host{name: "DS Stockholm", city: "Stockholm", country_code: "SE", time_zone: "Europe/Stockholm"})
+  host3 = Repo.insert!(%Host{name: "DS Dublin", city: "Dublin", country_code: "IE", time_zone: "Europe/Dublin"})
 
-# Create demo contests
+  # Create demo contests
 
-season = 54
-year = JumuParams.year(season)
-contest_attrs = %{
-  season: season,
-  round: 1,
-  start_date: %{day: 1, month: 1, year: year},
-  end_date: %{day: 2, month: 1, year: year},
-  signup_deadline: %{day: 15, month: 12, year: year - 1},
-  timetables_public: false
-}
+  season = 54
+  year = JumuParams.year(season)
+  contest_attrs = %{
+    season: season,
+    round: 1,
+    start_date: %{day: 1, month: 1, year: year},
+    end_date: %{day: 2, month: 1, year: year},
+    signup_deadline: %{day: 15, month: 12, year: year - 1},
+    timetables_public: false
+  }
 
-contests = [
-  Contest.changeset(%Contest{}, Map.put(contest_attrs, :host_id, host1.id)),
-  Contest.changeset(%Contest{}, Map.put(contest_attrs, :host_id, host2.id)),
-  Contest.changeset(%Contest{}, Map.put(contest_attrs, :host_id, host3.id)),
-  Contest.changeset(%Contest{}, Map.merge(contest_attrs, %{
-    host_id: host1.id,
-    round: 2,
-    start_date: %{day: 15, month: 3, year: year},
-    end_date: %{day: 17, month: 3, year: year},
-    signup_deadline: %{day: 28, month: 2, year: year},
-  }))
-]
-contests |> Enum.each(&Repo.insert!(&1))
+  contests = [
+    Contest.changeset(%Contest{}, Map.put(contest_attrs, :host_id, host1.id)),
+    Contest.changeset(%Contest{}, Map.put(contest_attrs, :host_id, host2.id)),
+    Contest.changeset(%Contest{}, Map.put(contest_attrs, :host_id, host3.id)),
+    Contest.changeset(%Contest{}, Map.merge(contest_attrs, %{
+      host_id: host1.id,
+      round: 2,
+      start_date: %{day: 15, month: 3, year: year},
+      end_date: %{day: 17, month: 3, year: year},
+      signup_deadline: %{day: 28, month: 2, year: year},
+    }))
+  ]
+  contests |> Enum.each(&Repo.insert!(&1))
+end
