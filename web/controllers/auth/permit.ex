215f4ee7285544b2f3a17417d6428c4cd8d0263d
Martin Richter <me@martinrichter.net>
2017-04-05 14:35:13 +0200

    Hide 'New contest' button when user cannot create contests

--- web/controllers/auth/permit.ex
+++ web/controllers/auth/permit.ex
@@ -2,6 +2,7 @@ defmodule Jumubase.Permit do
   use Jumubase.Web, :controller
   alias Jumubase.{Contest, Host, User}
 
+  # Action-based rules
   def authorize(_, _, %User{role: "admin"}), do: :ok
   def authorize(Contest, :index, %User{}), do: :ok
   def authorize(Contest, _, %User{}), do: {:error, :unauthorized}
@@ -14,6 +15,11 @@ defmodule Jumubase.Permit do
   def authorize(Host, _, %User{}), do: {:error, :unauthorized}
   def authorize(User, _, %User{}), do: {:error, :unauthorized}
 
+  def authorized?(target, action, user) do
+    authorize(target, action, user) == :ok
+  end
+
+  # Scope-based rules
   def accessible_by(query, %User{role: "admin"}), do: query
   def accessible_by(query, %User{} = user) do
     from c in query, where: c.host_id in ^host_ids(user)
